40775ba93239490bc76988113630f1185cd27c83
Wojciech Rogoz <wrogoz@gmail.com>
2020-08-18 10:44:28 +0200

    conn to db cl

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const chalk = require('chalk')
-mongoose.connect("mongodb+srv://test:test@dontforget.un0gw.mongodb.net/dontForget?retryWrites=true&w=majority" || 'mongodb://localhost:27017/dont-forget', {useNewUrlParser: true, useUnifiedTopology: true,useCreateIndex: true})
+mongoose.connect("mongodb+srv://test:test@dontForget.un0gw.mongodb.net/dontForget?retryWrites=true&w=majority" || 'mongodb://localhost:27017/dont-forget', {useNewUrlParser: true, useUnifiedTopology: true,useCreateIndex: true})
 .then(()=>{
     console.log(chalk.blue.underline.bold('db connected'))
 });
